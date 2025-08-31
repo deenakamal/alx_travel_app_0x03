91b4c9a1f683e057e006b1ce5a9228ea0100c9dc
Deena <133583413+deenakamal@users.noreply.github.com>
2025-08-31 23:51:27 +0300

    Update views.py

--- alx_travel_app/listings/views.py
+++ alx_travel_app/listings/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .tasks import send_booking_confirmation_email
 
-# Create your views here.
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+    def perform_create(self, serializer):
+        booking = serializer.save()
+        send_booking_confirmation_email.delay(
+            booking.user.email,
+            f'Booking ID: {booking.id}, Hotel: {booking.hotel.name}'
+        )
